460b89d5efbdc6f348c97271317527603d515482
BagusRezky <bagusrezky15@gmail.com>
2025-12-02 04:23:19 +0700

    feat: update AppLogo component to change displayed name to 'UG. RMS'

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -7,7 +7,7 @@ export default function AppLogo() {
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Indigama Khatulistiwa</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">UG. RMS</span>
             </div>
         </>
     );
